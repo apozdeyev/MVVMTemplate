d5c5f642cd314a40485b0482c213f7c35159b3a2
AnatoliyPozdeyev <anatoliy.pozdeyev@gmail.com>
2017-11-05 12:58:12 +0300

    Empty submodules.

---
+++ MVVMTemplate/Helpers/Helpers.h
@@ -0,0 +1,19 @@
+//
+//  Helpers.h
+//  Helpers
+//
+//  Created by Anatoliy Pozdeyev on 05/11/2017.
+//  Copyright © 2017 VitaSw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Helpers.
+FOUNDATION_EXPORT double HelpersVersionNumber;
+
+//! Project version string for Helpers.
+FOUNDATION_EXPORT const unsigned char HelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Helpers/PublicHeader.h>
+
+
---
+++ MVVMTemplate/Resources/Configs/View/View.h
@@ -0,0 +1,19 @@
+//
+//  View.h
+//  View
+//
+//  Created by Anatoliy Pozdeyev on 05/11/2017.
+//  Copyright © 2017 VitaSw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for View.
+FOUNDATION_EXPORT double ViewVersionNumber;
+
+//! Project version string for View.
+FOUNDATION_EXPORT const unsigned char ViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <View/PublicHeader.h>
+
+
